1cb9cad4913c666478fc14a1282204a0391cd486
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-09-11 16:24:13 +0530

    nav added

---
+++ components/common/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { IoReaderOutline } from "react-icons/io5";
+import { ImFontSize } from "react-icons/im";
+import { RiFontSize } from "react-icons/ri";
+import { BsCalendarCheck } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import logo from "../../common/assets/logo.svg";
+import pib from "../../common/assets/pib.svg";
+import azadi from "../../common/assets/azadi.svg";
+import g20 from "../../common/assets/g20.svg";
+
+export default function Navbar() {
+  return (
+    <section className="flex flex-col w-full h-full">
+      <section className="flex justify-between items-center px-4 text-lg">
+        <Image src={logo} alt="logo" height={80} width={80} />
+        <div className="flex justify-center items-center gap-x-6 max-md:hidden">
+          <li className="flex justify-center items-center gap-2">
+            <BsCalendarCheck size={25} />
+            <h1>Main Content</h1>
+          </li>
+          <li className="flex justify-center items-center gap-2">
+            <RiFontSize size={25} />
+            <h1>+AA -A</h1>
+          </li>
+          <li className="flex justify-center items-center gap-2">
+            <IoReaderOutline size={25} />
+            <h1>Screen Reader</h1>
+          </li>
+          <li className="flex justify-center items-center gap-2">
+            <ImFontSize size={25} />
+            <h1 className="flex justify-between items-center flex-row">
+              <span>English</span> <RiArrowDropDownLine />
+            </h1>
+          </li>
+        </div>
+        <GiHamburgerMenu className="md:hidden block" />
+      </section>
+      <hr className="w-full border-1 border-[#000000]" />
+      <section className="flex justify-evenly items-center w-full pb-2 pt-4">
+        <Image src={pib} alt="pib" height={130} width={130} />
+        <Image src={azadi} alt="azadi" height={100} width={100} />
+        <Image src={g20} alt="g20" height={90} width={90} />
+      </section>
+    </section>
+  );
+}
